a5bcb137c44f6a8743ea0d6c306d8399e9fb754d
Hamood <hamoodwahab@gmail.com>
2021-06-18 20:24:43 +0530

    First Commit

---
+++ JavaScript.js
@@ -0,0 +1,8 @@
+function viewall() {
+document.getElementById("all").style.display="flex";
+document.getElementById("new").style.display="none";
+}
+function viewnew() {
+    document.getElementById("new").style.display="flex";
+    document.getElementById("all").style.display="none";
+}
