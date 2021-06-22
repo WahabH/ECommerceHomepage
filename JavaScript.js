fbc7a4a76de97f2f399746e725ad1c4d5b5e5a04
Hamood <hamoodwahab@gmail.com>
2021-06-22 15:22:38 +0530

    Changes in tab section

--- JavaScript.js
+++ JavaScript.js
@@ -1,8 +1,12 @@
 function viewall() {
 document.getElementById("all").style.display="flex";
 document.getElementById("new").style.display="none";
+document.getElementById("tab1").style.color="red";
+document.getElementById("tab2").style.color="black";
 }
 function viewnew() {
     document.getElementById("new").style.display="flex";
     document.getElementById("all").style.display="none";
+    document.getElementById("tab2").style.color="red";
+    document.getElementById("tab1").style.color="black";
 }
